cf28cecb0da3e1b3305d5e405e0cbcb081cf0ff3
Dustin Huang <mail@dustinhuang.com>
2017-09-13 02:36:29 -0700

    Add stringifyJSON implementation

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,26 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null)
+    return 'null';
+  
+  if (typeof(obj) === 'number' || typeof(obj) === 'boolean')
+    return obj.toString();
+
+  if (typeof(obj) === 'string')
+    return '"' + obj + '"';
+
+  if (typeof(obj) === 'object') {
+    if (obj.constructor === Array) {
+      return '[' + obj.map(function (item) { return stringifyJSON(item) }) + ']';
+    } else { // Object
+      return '{' + 
+        Object.entries(obj).filter(function (item) {
+          return (item[0] !== 'undefined' && item[0] !== 'functions')
+        }).map(function (item) {
+          return stringifyJSON(item[0]) + ':' + stringifyJSON(item[1])
+        })
+      + '}';
+    }
+  }
 };
